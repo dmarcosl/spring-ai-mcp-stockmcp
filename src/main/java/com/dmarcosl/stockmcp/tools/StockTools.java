4c9091ccecaa015c498d1c517274706fd037a76f
Daniel Marcos <danielmarcos.to@gmail.com>
2025-11-13 00:29:19 +0100

    update

--- src/main/java/com/dmarcosl/stockmcp/tools/StockTools.java
+++ src/main/java/com/dmarcosl/stockmcp/tools/StockTools.java
@@ -2,10 +2,12 @@ package com.dmarcosl.stockmcp.tools;
 
 import com.dmarcosl.stockmcp.domain.StockService;
 import lombok.RequiredArgsConstructor;
+import org.springaicommunity.mcp.annotation.McpTool;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -15,22 +17,22 @@ public class StockTools {
 
   private final StockService stockService;
 
-  @Tool(
+  @McpTool(
       name = "all_stocks",
       description = "Returns quantity for each product")
-  public List<StockService.StockSummary> allStocks() {
-    return stockService.getAllStockSummaries().collectList().block();
+  public Mono<List<StockService.StockSummary>> allStocks() {
+    return stockService.getAllStockSummaries().collectList();
   }
 
-  @Tool(
+  @McpTool(
       name = "stock_by_product",
       description = "Returns quantity for a given product")
-  public StockService.StockSummary stockByProduct(
+  public Mono<StockService.StockSummary> stockByProduct(
       @ToolParam(description = "Name of the product (i.e. 'skirt')") String name,
       @ToolParam(description = "Color of the product (i.e. 'pink')") String color) {
     if (!StringUtils.hasText(name) || !StringUtils.hasText(color)) {
       throw new IllegalArgumentException("Missing parameters: name and color");
     }
-    return stockService.getStockSummaryByProduct(name, color).block();
+    return stockService.getStockSummaryByProduct(name, color);
   }
 }
